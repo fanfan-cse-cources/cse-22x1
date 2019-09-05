1f9856b69a3b7c9048708fe8544527517cbdd657
Yifan Yao <fanfannnmn@gmail.com>
2019-09-05 16:33:13 -0400

    Add test cases for NaturalNumberOnString

--- NaturalNumberOnString/test/NaturalNumberTest.java
+++ NaturalNumberOnString/test/NaturalNumberTest.java
@@ -1,4 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
 
 /**
  * JUnit test fixture for {@code NaturalNumber}'s constructors and kernel
@@ -97,6 +102,164 @@ public abstract class NaturalNumberTest {
      */
     protected abstract NaturalNumber constructorRef(NaturalNumber n);
 
-    // TODO - add test cases for four constructors, multiplyBy10, divideBy10, isZero
+    @Test
+    public final void testNoArgumentConstructor() {
+        NaturalNumber n = this.constructorTest();
+        NaturalNumber expected = this.constructorRef();
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testIntConstructor() {
+        NaturalNumber n = this.constructorTest(1);
+        NaturalNumber expected = this.constructorRef(1);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testStringConstructor() {
+        NaturalNumber n = this.constructorTest("1");
+        NaturalNumber expected = this.constructorRef("1");
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testNaturalNumberConstructor() {
+        NaturalNumber num = new NaturalNumber2(1);
+
+        NaturalNumber n = this.constructorTest(num);
+        NaturalNumber expected = this.constructorRef(num);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testEmptyMultiplyBy10() {
+        NaturalNumber n = this.constructorTest();
+        NaturalNumber expected = this.constructorRef(1);
+
+        n.multiplyBy10(1);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testNumberMultiplyBy10() {
+        NaturalNumber n = this.constructorTest(10);
+        NaturalNumber expected = this.constructorRef(100);
+
+        n.multiplyBy10(0);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testLargeNumberMultiplyBy10() {
+        NaturalNumber n = this.constructorTest(15);
+        NaturalNumber expected = this.constructorRef(150);
+
+        n.multiplyBy10(0);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testLargestIntNumberMultiplyBy10() {
+        NaturalNumber n = this.constructorTest(2147483647);
+        NaturalNumber expected = this.constructorRef("21474836470");
+
+        n.multiplyBy10(0);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testLargestIntNumberMultiplyBy10Plus() {
+        NaturalNumber n = this.constructorTest(2147483647);
+        NaturalNumber expected = this.constructorRef("21474836475");
+
+        n.multiplyBy10(5);
+
+        assertEquals(expected, n);
+    }
+
+    @Test
+    public final void testNumberDivideBy10() {
+        NaturalNumber n = this.constructorTest(10);
+        NaturalNumber expected = this.constructorRef(1);
+
+        int x = n.divideBy10();
+        int expectedX = 0;
+
+        assertEquals(expected, n);
+        assertEquals(expectedX, x);
+    }
+
+    @Test
+    public final void testNumberDivideBy10WithNonZero() {
+        NaturalNumber n = this.constructorTest(101);
+        NaturalNumber expected = this.constructorRef(10);
+
+        int x = n.divideBy10();
+        int expectedX = 1;
+
+        assertEquals(expected, n);
+        assertEquals(expectedX, x);
+    }
+
+    @Test
+    public final void testNumberDivideBy10ToEmpty() {
+        NaturalNumber n = this.constructorTest(5);
+        NaturalNumber expected = this.constructorRef();
+
+        int x = n.divideBy10();
+        int expectedX = 5;
+
+        assertEquals(expected, n);
+        assertEquals(expectedX, x);
+    }
+
+    @Test
+    public final void testLargestIntNumberDivideBy10() {
+        NaturalNumber n = this.constructorTest(2147483647);
+        NaturalNumber expected = this.constructorRef("214748364");
+
+        int x = n.divideBy10();
+        int expectedX = 7;
+
+        assertEquals(expected, n);
+        assertEquals(expectedX, x);
+    }
+
+    @Test
+    public final void testMoreThanLargestIntNumberDivideBy10() {
+        NaturalNumber n = this.constructorTest("21474836475");
+        NaturalNumber expected = this.constructorRef("2147483647");
+
+        int x = n.divideBy10();
+        int expectedX = 5;
+
+        assertEquals(expected, n);
+        assertEquals(expectedX, x);
+    }
+
+    @Test
+    public final void testIsZeroTrue() {
+        NaturalNumber n = this.constructorTest();
+        NaturalNumber expected = this.constructorRef();
+
+        assertEquals(expected.isZero(), n.isZero());
+    }
+
+    @Test
+    public final void testIsZeroFalse() {
+        NaturalNumber n = this.constructorTest(5);
+        NaturalNumber expected = this.constructorRef(5);
+
+        assertEquals(expected.isZero(), n.isZero());
+    }
 
 }
